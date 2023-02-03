2460acfeb3da1058b274ac01eb68b00f0d46d202
Divyansh Singh <divyanshsinghhacked11@gmail.com>
2023-02-03 23:21:55 +0530

    In this repo what i learn about the JavaScript

---
+++ Swap the Value/Solution.js.js
@@ -0,0 +1,12 @@
+var a = "5";
+var b = "10";    
+/*Do not change the code above*/
+
+//Write your code here
+var temp = a;
+a = b;
+b = temp;
+/***********Do not change the code below 👇*******/
+console.log("a is " + a);
+console.log("b is " + b);
+
---
+++ Variable/variable.js
@@ -0,0 +1,10 @@
+var a =5;
+var b =10;
+// swap the both number
+let t = 23;
+t=34;//update can be happen
+//but let  t  = 54; Redelaritions not possible
+
+const m =  55;//read and redeclation will possible at same time only in const
+console.log("a "+ a); 
+console.log("b "+ b); 
