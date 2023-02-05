ba3a6fa8a06b999deaa416d0f1f3051fcd077ab8
Divyansh Singh <divyanshsinghhacked11@gmail.com>
2023-02-05 13:35:37 +0530

    in this i cover Hosting in Js

---
+++ Hosting/main.js
@@ -0,0 +1,17 @@
+
+hostName();//hello to alls
+console.log(x);//undefine
+console.log(hostName);//written whole function
+var x = 56;
+
+function hostName(){
+    console.log("hello to alls");
+}
+
+// console.log(hostName);
+
+t = 5;
+console.log(t);
+// var t;
+// typeof(t);
+console.log(typeof(t));
